d4c9ccb16f233768de458ff86cc78403211c4c0b
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-12-09 10:34:09 +0100

    fix(provider/facebook): make enableEventEnhancement & extendCookies configurable

--- pkg/config/facebook.go
+++ pkg/config/facebook.go
@@ -4,14 +4,27 @@ import (
 	"github.com/foomo/gocontemplate/pkg/contemplate"
 )
 
-type Facebook struct {
-	// Enable provider
-	Enabled        bool   `json:"enabled" yaml:"enabled"`
-	PixelID        string `json:"pixelId" yaml:"pixelId"`
-	APIAccessToken string `json:"apiAccessToken" yaml:"apiAccessToken"`
-	TestEventToken string `json:"testEventToken" yaml:"testEventToken"`
-	// Google Consent settings
-	GoogleConsent GoogleConsent `json:"googleConsent" yaml:"googleConsent"`
-	// Google Tag Manager server container settings
-	ServerContainer contemplate.Config `json:"serverContainer" yaml:"serverContainer"`
+type (
+	Facebook struct {
+		// Enable provider
+		Enabled        bool   `json:"enabled" yaml:"enabled"`
+		PixelID        string `json:"pixelId" yaml:"pixelId"`
+		APIAccessToken string `json:"apiAccessToken" yaml:"apiAccessToken"`
+		TestEventToken string `json:"testEventToken" yaml:"testEventToken"`
+		// Google Consent settings
+		GoogleConsent GoogleConsent `json:"googleConsent" yaml:"googleConsent"`
+		// Google Tag Manager server container settings
+		ServerContainer FacebookServerContainer `json:"serverContainer" yaml:"serverContainer"`
+	}
+	FacebookServerContainer struct {
+		contemplate.Config `json:",inline" yaml:",squash"`
+		Settings           map[string]FacebookConversionAPITag `json:"settings" yaml:"settings"`
+	}
+)
+
+func (s *FacebookServerContainer) Setting(eventName string) FacebookConversionAPITag {
+	if value, ok := s.Settings[eventName]; ok {
+		return value
+	}
+	return FacebookConversionAPITag{}
 }
